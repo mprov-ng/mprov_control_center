40627f4299ee0c03d614bb96c6822d0ddeb5954c
Jason Williams <jasonw@jhu.edu>
2022-05-05 10:14:49 -0400

    Removing some cruft.

--- mprov_control_center/settings.py
+++ mprov_control_center/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'core.common',
-    'locations',
+    #'locations',
     'networks',
     'osmanagement',
     'systems',
@@ -110,38 +110,6 @@ JAZZMIN_UI_TWEAKS = {
     },
     "actions_sticky_top": False
 }
-# JAZZMIN_UI_TWEAKS = {
-#     "navbar_small_text": True,
-#     "footer_small_text": False,
-#     "body_small_text": False,
-#     "brand_small_text": False,
-#     "brand_colour": "navbar-success",
-#     "accent": "accent-success",
-#     "navbar": "navbar-success navbar-dark",
-#     "no_navbar_border": False,
-#     "navbar_fixed": False,
-#     "layout_boxed": False,
-#     "footer_fixed": False,
-#     "sidebar_fixed": False,
-#     "sidebar": "sidebar-light-success",
-#     "sidebar_nav_small_text": True,
-#     "sidebar_disable_expand": True,
-#     "sidebar_nav_child_indent": False,
-#     "sidebar_nav_compact_style": False,
-#     "sidebar_nav_legacy_style": False,
-#     "sidebar_nav_flat_style": False,
-#     "theme": "default",
-#     "dark_mode_theme": None,
-#     "button_classes": {
-#         "primary": "btn-primary",
-#         "secondary": "btn-secondary",
-#         "info": "btn-outline-info",
-#         "warning": "btn-outline-warning",
-#         "danger": "btn-outline-danger",
-#         "success": "btn-outline-success"
-#     },
-#     "actions_sticky_top": False
-# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
