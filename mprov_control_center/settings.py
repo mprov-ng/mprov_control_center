37d89adfb21610ba764c5281485b79dee63a21cc
Jason Williams <jasonw@jhu.edu>
2022-05-12 09:32:48 -0400

    Update static files and db locations, prepare for deployment.

--- mprov_control_center/settings.py
+++ mprov_control_center/settings.py
@@ -152,7 +152,7 @@ WSGI_APPLICATION = 'mprov_control_center.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
         'OPTIONS': {
             'timeout': 20,
         },
@@ -208,12 +208,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"),
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,"static"),
     #'/var/www/static/',
-]
+#]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
