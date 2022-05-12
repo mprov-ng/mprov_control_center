b308b8908ece104a2e54c93989236863fb65327c
Jason Williams <jasonw@jhu.edu>
2022-05-12 16:25:37 -0400

    adding script windows to unix conversion

--- scripts/models.py
+++ scripts/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from django.utils.text import slugify
+from django.conf import settings
+import os
 
 
 class ScriptType(models.Model):
@@ -29,6 +31,12 @@ class Script(models.Model):
       self.slug = slugify(self.filename.name)
     self.filename.name = self.slug + "-v" + str(self.version)
     super(Script, self).save(*args, **kwargs)
+    print(os.path.join(settings.MEDIA_ROOT, self.filename.name))
+    if os.path.exists(os.path.join(settings.MEDIA_ROOT, self.filename.name)):
+      print("Converting file")
+      os.system("dos2unix " + os.path.join(settings.MEDIA_ROOT, self.filename.name))
+
+  
 
 
 @receiver(post_delete, sender=Script)
