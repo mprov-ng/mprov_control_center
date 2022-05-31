6b07f060f940c53f4c06333c56c72c8938559d2c
Jason Williams <jasonw@jhu.edu>
2022-05-31 14:33:39 -0400

    Start work on an installl script for mpcc

--- INSTALL.md
+++ INSTALL.md
@@ -10,8 +10,15 @@ You will also need:
 - A database of some kind that will work with Django
 - A web server of some design
 
-In addition ot the items listed above, you will also need the python packages listed in the requirements.txt file.  You can easily install them after cloning the repository with the `pip install -r requirements.txt` command.
+It is recommended to use a python venv for the mPCC.  You can easily set one up with `python3.8 -m venv .` to create a python 3.8 venv in the current directory.
+
+In addition ot the items listed above, you will also need the python packages listed in the requirements.txt file.  You can easily install them after cloning the repository with the `pip install -r requirements.txt` command or if you use the `install_mpcc.sh` script below, it will be done for you.
 
 Installation of most of the prerequisit software can be accomplished with the following dnf command:
 ```# dnf -y install python38-mod_wsgi.x86_64 ipmitool dnsmasq ipxe-bootimgs.noarch jq golang```
+Note: The `install_mpcc.sh` will do this for you.
+
+
+=== Installation ===
+It is HIGHLY recommended to use the `install_mpcc.sh` script if you are installing into a RHEL-8 derivative (ie: Rocky Linux 8).
 
---
+++ install_mpcc.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+dnf -y install epel-release
+dnf -y install python38-mod_wsgi.x86_64 ipmitool dnsmasq ipxe-bootimgs jq golang git 
+cd /var/www/
+git clone git@github.com:mProv-Manager/mprov_control_center.git
+cd mprov_control_center
+python3.8 -m venv .
+. bin/activate
+pip install -r requirements.txt
+
+# generates a secret from django's utils
+export SECRET_KEY=`python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'`
+echo "SECRET_KEY='$SECRET_KEY'" > .env
+
+echo "DJANGO_SUPERUSER_USERNAME=admin" >> .env
+echo "DJANGO_SUPERUSER_PASSWORD=admin" >> .env
+echo "DJANGO_SUPERUSER_EMAIL=root@localhost" >> .env
+
+ALLOWED_HOSTS=""
+for i in `ip addr | grep "inet " | awk '{print $2}' | awk -F/ '{print $1}'`
+do
+        ALLOWED_HOSTS="${i},${ALLOWED_HOSTS}"
+done
+echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> .env
+
+
+# other variables can be set directly in the ENV (for containers)
+# or via the .env file.   See the .env.example for possible varialbes.
+
+mkdir -p db/
+
+python ./manage.py migrate
+python ./manage.py  createsuperuser --noinput
+
+python ./manage.py loaddata */fixtures/*
+python manage.py collectstatic --noinput
+
+
+chgrp apache db/ -R
+chmod g+sw db/ -R
+
+
+# set up the apache stuff
+cd /etc/httpd/conf.d/
+cat << EOF > mprov_command_center.conf
+WSGIScriptAlias / /var/www/mprov_control_center/mprov_control_center/wsgi.py
+WSGIPythonHome /var/www/mprov_control_center/
+WSGIPythonPath /var/www/mprov_control_center/
+WSGIDaemonProcess mprov.arch.jhu.edu processes=2 threads=15 lang='en_US.UTF-8' locale='en_US.UTF-8' python-home=/var/www/mprov_control_center/ python-path=/var/www/mprov_control_center/
+WSGIProcessGroup mprov.arch.jhu.edu
+WSGIPassAuthorization On
+
+<Directory /var/www/mprov_control_center/mprov_control_center/>
+        <Files wsgi.py>
+                Require all granted
+        </Files>
+</Directory>
+
+Alias /robots.txt /var/www/mprov_control_center/static/robots.txt
+Alias /favicon.ico /var/www/mprov_control_center/static/favicon.ico
+
+Alias /media/ /var/www/mprov_control_center/media/
+Alias /static/ /var/www/mprov_control_center/static/
+
+<Directory /var/www/mprov_control_center/static>
+Require all granted
+</Directory>
+
+<Directory /var/www/mprov_control_center/media>
+Require all granted
+</Directory>
+
+<Location "/secret">
+    AuthType Basic
+    AuthName "Top Secret"
+    Require valid-user
+    AuthBasicProvider wsgi
+    WSGIAuthUserScript /var/www/mprov_control_center/mprov_control_center/wsgi.py
+</Location>
+EOF
+
+systemctl enable httpd
+systemctl restart httpd
+firewall-cmd --zone=public --add-service=http
+firewall-cmd --zone=public --add-service=http --permanent
