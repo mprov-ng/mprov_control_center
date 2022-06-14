c024adcd7a1c1554e3384f663adce8974959de71
Jason Williams <jasonw@jhu.edu>
2022-06-14 11:13:31 -0400

    Adding a kludge for an odd intermittent bug.

--- systems/serializers.py
+++ systems/serializers.py
@@ -1,3 +1,4 @@
+from array import array
 from pyexpat import model
 from systems.models import NetworkInterface, SystemImage, System, SystemGroup
 from rest_framework import serializers
@@ -27,17 +28,37 @@ class SystemImageSerializer(serializers.ModelSerializer):
         model = SystemImage
         fields = '__all__'
 
+    def validate_jobservers(self, value):
+        print(f"Prevalidation jobservers:'{value}'")
+        return value
+    
+    def check_jobservers(self, validated_data):
+        if 'jobservers' in self.initial_data and not 'jobservers' in validated_data:
+            print(f"ODD BUG DETECTED!  FIXING JOBSERVERS!")
+            validated_data['jobservers'] = []
+            for jsid in self.initial_data['jobservers']:
+                # look up the job server
+                js = JobServer.objects.get(pk=jsid)
+                validated_data['jobservers'].append(js)
+        print(f"check_jobservers: {validated_data}")
+
 
     def update(self, instance, validated_data):
+        print(f"Dumping data: {self.initial_data}")
+
+        self.check_jobservers(validated_data)
         print(f"{validated_data}")
-        jobservers_data = validated_data.pop('jobservers')
-        print("JS Data: " + str(jobservers_data))
         image = super().update(instance, validated_data)
         image.save()
+        if 'jobservers' in validated_data:
+            jobservers_data = validated_data.pop('jobservers')
+            print("JS Data: " + str(jobservers_data))
+            
 
-        for jobserver in jobservers_data:
-            image.jobservers.add(jobserver.pk)
-        
+            for jobserver in jobservers_data:
+                image.jobservers.add(jobserver.pk)
+        else:
+            print(f"No jobserver in update, not changing.")
         return image
 class NetworkInterfaceSerializer(serializers.ModelSerializer):
     class Meta:
