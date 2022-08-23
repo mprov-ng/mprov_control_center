97968070c74f239000627aff05c5c999ee98055c
Jason Williams <jasonw@jhu.edu>
2022-08-23 14:18:54 -0400

    add help block to inline forms.

--- disklayouts/admin.py
+++ disklayouts/admin.py
@@ -26,6 +26,7 @@ class DiskPartitionInline(admin.TabularInline):
   model = DiskPartition
   extra = 1
   ordering = ['partnum']
+  description = "When adding physical partitions that will be used as software RAID members, add the word 'raid' to mount point AND filesystem."
   
 
 class DiskLayoutAdmin(AdminWizardModel):
