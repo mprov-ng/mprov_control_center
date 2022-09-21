c91c3441c14a7bc6761299bbc5ab31fac32ce5d1
Jason Williams <jasonw@jhu.edu>
2022-09-21 16:41:00 -0400

    change default jobserver port to 8080

--- jobqueue/models.py
+++ jobqueue/models.py
@@ -43,7 +43,7 @@ class JobServer(models.Model):
     endpoint="/jobservers/"
     name = models.CharField(max_length=255, unique=True)
     address = models.GenericIPAddressField()
-    port = models.IntegerField(verbose_name="Port", default=80, null=True)
+    port = models.IntegerField(verbose_name="Port", default=8080, )
     heartbeat_time = models.DateTimeField(auto_now=True, verbose_name="Last Heart Beat")
     jobmodules=models.ManyToManyField(JobModule, verbose_name="Handled Job Modules")
     one_minute_load=models.FloatField(verbose_name="1 Minute Load Avg.", default=0.00, null=True)
