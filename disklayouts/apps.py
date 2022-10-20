7873cee80f2a1965fa31a9571ed570ef17b6a4b8
Jason Williams <jasonw@jhu.edu>
2022-10-20 14:32:28 -0400

    Fix name for disk layouts

--- disklayouts/apps.py
+++ disklayouts/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class DisklayoutsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    verbose_name = 'Stateful Disk Layouts'
+    verbose_name = 'System Disk Layouts'
     name = 'disklayouts'
