21fc61b304dc9cc309b2916907e71a3737b02044
Jason Williams <jasonw@jhu.edu>
2023-03-28 08:47:57 -0400

    Rename "Scripts" to "Config Management"

--- scripts/apps.py
+++ scripts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ScriptsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scripts'
+    verbose_name="Config Management"
