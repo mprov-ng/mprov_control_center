0f40320e9329fa37a8ba5b8666c0a166aa10b822
Jason Williams <jasonw@jhu.edu>
2023-06-12 14:08:24 -0400

    a script to install the gpu drivers

---
+++ install_scripts/nvidia_gpu_driver_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$NO_NVIDIA" ]
+then
+  exit 0
+fi
+
+
+role="git+https://github.com/NVIDIA/ansible-role-nvidia-driver.git"
+
+extravars="nvidia_driver_skip_reboot=yes"
+
+mkdir -p /tmp/mprov/ansible/roles/
+ansible-galaxy role install -p /tmp/mprov/ansible/roles/ $role -f
+# run the role, hope you set your variables.....
+if [[ $role == git* ]]
+then
+  role=`basename -s .git $role`
+fi
+
+cat <<- EOF > /tmp/role.yaml
+- hosts: localhost
+  become: true
+  gather_facts: true
+  tasks:
+    - name: Import role $role
+      ansible.builtin.include_role:
+        name: $role
+EOF
+ANSIBLE_ROLES_PATH="/tmp/mprov/ansible/roles" ANSIBLE_GATHER=implicit ansible-playbook -i localhost, -c local -e "${extravars}" /tmp/role.yaml
