efdcf3b7397f3dd1910adb37dbb3004db61d740b
Jason Williams <jasonw@jhu.edu>
2023-07-17 16:35:37 -0400

    fix the path setup for mprov net mount.

--- install_scripts/create_mprov_share.sh
+++ install_scripts/create_mprov_share.sh
@@ -13,13 +13,24 @@ fi
 mkdir -p /opt/mprov
 
 # Add bind mount to fstab
+touch /etc/fstab
 grep -qF '/export/mprov' /etc/fstab || echo '/export/mprov    /opt/mprov    none defaults,bind    0 0' >> /etc/fstab
 
+# add it to path
+cat << EOF > /etc/profile.d/99-mprov.sh
+export PATH=/opt/mprov/bin:$PATH
+
+if [ "\$USER" == "root" ]
+then
+  export PATH=/opt/mprov/sbin/:/opt/mprov/bin:$PATH
+fi
+EOF
 
 # mount the bind mount
 mount /opt/mprov
 
 # Add the export to /etc/exports
+touch /etc/exports
 grep -qF '/export/mprov' /etc/exports || echo '/export/mprov    *(rw,no_root_squash,sync)' >> /etc/exports
 
 # install the nfs server stuff
