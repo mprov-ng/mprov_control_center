99b1d53f395f78d0731078186fe82e3d2d1dc15e
Jason Williams <jasonw@jhu.edu>
2023-11-08 15:07:52 -0500

    rage.

--- install_scripts/install_mpcc.sh
+++ install_scripts/install_mpcc.sh
@@ -112,7 +112,7 @@ then
         cat << EOF | mysql -u root mysql
         create database $DB_NAME;
 EOF
-        echo "drop user $DB_USER;" | mysql -u root mysql
+        echo "drop user '$DB_USER'@'localhost';" | mysql -u root mysql
         echo "flush privileges; " | mysql -u root mysql
         cat << EOF | mysql -u root mysql
         CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
