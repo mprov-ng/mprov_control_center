37030bfef05249300f80d6843a0b3f33efb023a0
Jason Williams <54407063+jhujasonw@users.noreply.github.com>
2023-11-13 16:08:19 -0500

    run the mprov_jobserver install in a clean env.

--- install_scripts/install_initial_jobserver.sh
+++ install_scripts/install_initial_jobserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-
-wget https://raw.githubusercontent.com/mprov-ng/mprov_jobserver/main/install_mprov_jobserver.sh -O - | bash
+# clear the env.
+wget https://raw.githubusercontent.com/mprov-ng/mprov_jobserver/main/install_mprov_jobserver.sh -O - | sudo su - -c /bin/bash
 
