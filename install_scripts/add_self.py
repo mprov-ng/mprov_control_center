90fc3dc256cf575f20107d7a90bfb2a21d3edeec
Jason Williams <jasonw@jhu.edu>
2023-11-14 13:36:05 -0500

    add mpcc host to self.

---
+++ install_scripts/add_self.py
@@ -0,0 +1,34 @@
+#!/var/www/mprov_control_center/manage.py shell
+from systems.models import System, NetworkInterface
+from django.contrib.auth.models import User
+
+
+import socket
+import netifaces as ni
+
+def get_ip_address(ifname):
+  try:
+    ip=str(ni.ifaddresses(ifname)[ni.AF_INET][0]['addr'])
+    print(ip)
+    return ip
+
+  except:
+    print("Nope")
+    return None
+
+# get the admin user
+user=User.objects.get(pk=1)
+
+# get our interfaces.
+if_list = socket.if_nameindex()
+
+selfSys = System.objects.create(hostname=socket.gethostname(), created_by=user,updated='1970-01-01T00:00+00:00',timestamp='1970-01-01T00:00+00:00')
+selfSys.save()
+
+for intf in if_list:
+  if intf[1] == "lo":
+    continue
+  intfip=get_ip_address(intf[1])
+  netinf = NetworkInterface.objects.create(hostname=socket.gethostname(), name=intf[1], system=selfSys, ipaddress=intfip)
+  netinf.save()
+
