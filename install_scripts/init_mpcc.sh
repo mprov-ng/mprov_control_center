99a330dd2a714ce1a85c7fa6d3c9acf93a297a5f
Jason Williams <jasonw@jhu.edu>
2024-12-12 01:17:29 -0500

    move secret generation to within the initalized check

--- install_scripts/init_mpcc.sh
+++ install_scripts/init_mpcc.sh
@@ -2,9 +2,6 @@
 cd /var/www/mprov_control_center
 . bin/activate
 
-# set up the secret key
-SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
-echo "SECRET_KEY=$SECRET_KEY" >> /var/www/mprov_control_center/.env
 
 python manage.py migrate
 python manage.py createsuperuser --noinput
@@ -13,6 +10,9 @@ python manage.py loaddata */fixtures/*
 
 if [ "$1" != "-d" ] 
 then
+       # set up the secret key
+       SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
+       echo "SECRET_KEY=$SECRET_KEY" >> /var/www/mprov_control_center/.env
 
        # Run the scripts from install_scripts/install.d/ in order
        for i in `ls /var/www/mprov_control_center/install_scripts/init.d | sort -n`
@@ -22,6 +22,10 @@ then
 else
        if [ ! -e /var/www/mprov_control_center/db/.initialized ]
        then
+              # set up the secret key
+              SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
+              echo "SECRET_KEY=$SECRET_KEY" >> /var/www/mprov_control_center/.env
+
               /var/www/mprov_control_center/install_scripts/load_default_scripts.sh
               touch /var/www/mprov_control_center/db/.initialized
        fi
