ffe889394593f58ea8934aebbb0e1742590287c8
Jason Williams <54407063+jhujasonw@users.noreply.github.com>
2024-12-12 22:36:58 -0500

    Add docker init code

--- install_scripts/init_mpcc.sh
+++ install_scripts/init_mpcc.sh
@@ -7,12 +7,21 @@ python manage.py createsuperuser --noinput
 
 python manage.py loaddata */fixtures/*
 
-# Run the scripts from install_scripts/install.d/ in order
-for i in `ls /var/www/mprov_control_center/install_scripts/init.d | sort -n`
-do
-       /var/www/mprov_control_center/install_scripts/init.d/$i
-done
+if [ "$1" != "-d" ] 
+then
 
+       # Run the scripts from install_scripts/install.d/ in order
+       for i in `ls /var/www/mprov_control_center/install_scripts/init.d | sort -n`
+       do
+              /var/www/mprov_control_center/install_scripts/init.d/$i
+       done
+else
+       if [ ! -e /var/www/mprov_control_center/static/.initialized ]
+       then
+              /var/www/mprov_control_center/install_scripts/load_default_scripts.sh
+              touch /var/www/mprov_control_center/static/.initialized
+       fi
+fi
 
 # fix the db perms... again...
 chown apache db/ -R
