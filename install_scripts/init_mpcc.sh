f861726831cbd13e04f4a463c4f6e3a867f3c1bf
Jason Williams <jasonw@jhu.edu>
2024-12-23 22:41:33 -0500

    work on container init at startup

--- install_scripts/init_mpcc.sh
+++ install_scripts/init_mpcc.sh
@@ -11,6 +11,9 @@ python manage.py loaddata */fixtures/*
 SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
 echo "SECRET_KEY=$SECRET_KEY" >> /var/www/mprov_control_center/.env
 
+# let's adjust the env file based on the docker env
+echo "ALLOWED_HOSTS=$ALLOWED_HOSTS,$MPROV_MPCC_HOSTNAME,${MPROV_MPCC_HOSTNAME}.${MPROV_DOMAINNAME},$MPROV_MPCC_IPs" >> /var/www/mprov_control_center/.env
+
 if [ "$1" != "-d" ] 
 then
 
@@ -22,6 +25,42 @@ then
 else
        if [ ! -e /var/www/mprov_control_center/db/.initialized ]
        then
+
+              # try to generate some interface commands based on ENV vars.
+              IFACE_CMDS=""
+              counter="0"
+              OLD_IFS=$IFS
+              IFS=","
+              for ip in $MPROV_MPCC_IPs
+              do
+                     counter=$((counter+1))
+                     IFACE_CMDS=$(
+                     cat <<-EOV 
+$IFACE_CMDS
+netinf = NetworkInterface.objects.create(hostname="${MPROV_MPCC_HOSTNAME}", name="eno$counter", system=selfSys, ipaddress="${ip}") 
+netinf.save()
+EOV
+                     )
+
+              done
+
+              IFS=$OLD_IFS
+
+              # add our host to the mpcc
+              . /var/www/mprov_control_center/bin/activate
+              cat <<EOF | /var/www/mprov_control_center/manage.py shell
+from systems.models import System, NetworkInterface
+from django.contrib.auth.models import User
+
+# get the admin user
+user=User.objects.get(pk=1)
+
+selfSys = System.objects.create(hostname="${MPROV_MPCC_HOSTNAME}", created_by=user,updated='1970-01-01T00:00+00:00',timestamp='1970-01-01T00:00+00:00')
+selfSys.save()
+
+${IFACE_CMDS}
+
+EOF
 
 
               /var/www/mprov_control_center/install_scripts/load_default_scripts.sh
