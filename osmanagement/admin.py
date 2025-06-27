96348b1678828a9d39f28587ab15cd360c3f87f3
Jason Williams <jasonw@jhu.edu>
2025-06-27 11:11:55 -0400

    Add bulk update option for os repos.

--- osmanagement/admin.py
+++ osmanagement/admin.py
@@ -1,4 +1,6 @@
 from statistics import mode
+from django.utils.text import slugify
+from jobqueue.models import JobModule, JobStatus, Job
 from django.contrib import admin
 from .forms import OSDistro_form
 from .models import (
@@ -6,11 +8,41 @@ from .models import (
   OSRepo,
 
 )
+@admin.action(description="Mark repositories as needing update.")
+def mark_update(modeladmin, request, queryset):
+  queryset.update(update=True)
+  for instance in queryset:
+    if not instance.update: 
+      return
+
+    JobType = None
+    try:
+        JobType = JobModule.objects.get(slug='repo-update')
+    except:
+        JobType = None
+    # print(str(JobType) + " post")
+
+    # get the jobtype, do nothing if it's not defined.
+    if JobType is not None:
+
+        # save a new job, if one doesn't already exist.
+        params = { 'repo_id': instance.id}
+        Job.objects.create( name=JobType.name, 
+            module=JobType, 
+            status=JobStatus.objects.get(pk=1), 
+            params = params,
+        )
+
+        # Increment version number and clear out jobservers field.
+        instance.hosted_by.clear()
+        instance.version = instance.version + 1
+
 class OSRepoAdmin(admin.ModelAdmin):
   model=OSRepo
   list_display=['id', 'name', 'repo_package_url', 'managed', 'version']
   list_display_links=['id','name']
   readonly_fields=['hosted_by']
+  actions = [mark_update]
 
 class OSDistroAdmin(admin.ModelAdmin):
   model = OSDistro,
