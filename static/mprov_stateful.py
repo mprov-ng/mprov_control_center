769e1e144c827ae81cde8843a02a19db67bc1cdc
Jason Williams <jasonw@jhu.edu>
2025-10-22 14:56:34 -0400

    Loloopscoptor

--- static/mprov_stateful.py
+++ static/mprov_stateful.py
@@ -21,7 +21,7 @@ import subprocess
 # - mark the system's bootable NIC as not bootable to indicate it's provisioned
 # - hand control over to systemd or reboot
 
-def exception_hook(exctype, value, tracebak):
+def exception_hook(exctype, value, traceback):
   traceback.print_exc()
   print("\n\nERROR: Installer failed.  Dropping to shell.  Note: You are still in the RAM disk.")
   os.execv("/bin/setsid" "/bin/bash -m  <> /dev/tty1 >&0 2>&1")
@@ -443,7 +443,7 @@ class mProvStatefulInstaller():
       sh.chroot([f"/newroot", f"grub2-mkconfig", f"-o", "/boot/grub2/grub2-efi.cfg"])
     else:
       print("Configuring GRUB2 BIOS Setup...")
-      with open("/etc/default/grub", "a") as gd:
+      with open("/newroot/etc/default/grub", "a") as gd:
         gd.write("GRUB_ENABLE_BLSCFG=false")
       sh.chroot([
         f"/newroot", 
