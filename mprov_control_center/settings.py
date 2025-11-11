1ef6be3e53a5a7debb7b1cfb700f29f085e6bd35
Jason Williams <jasonw@jhu.edu>
2025-11-11 14:43:46 +0000

    Change the cache backend to db and update init_mpcc.sh

--- mprov_control_center/settings.py
+++ mprov_control_center/settings.py
@@ -289,9 +289,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
 CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "mprov_cache",  # Name of your cache table
     }
 }
 
