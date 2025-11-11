efd08612ca5e6a8baccd0d46cee85814c014e6d2
Jason Williams <jasonw@jhu.edu>
2025-11-11 14:39:24 +0000

    add a devel mode to the init script.

--- install_scripts/init_mpcc.sh
+++ install_scripts/init_mpcc.sh
@@ -1,4 +1,71 @@
 #!/bin/bash
+if [ "$MPROV_DEVEL" == "1" ]
+then
+       echo "Running in development mode"
+       if [ ! -d /var/www/mprov_control_center/ ]
+       then
+              echo "Error: No development mount found!" >&2
+              exit 1
+
+       fi
+       if [ ! -e /var/www/mprov_control_center/.dev.init ]
+       then
+              # re-run the install stuff because we are running in dev mode first time.
+              cd /var/www/mprov_control_center
+              # git clone https://github.com/mprov-ng/mprov_control_center.git
+              # chmod 755 install_scripts/init_mpcc.sh
+              python3 -m venv .
+              . bin/activate
+              pip install --upgrade pip
+              pip install -r requirements.txt
+              pip install mysqlclient 
+              # TODO: All these entries should be checking if they exist first.
+              # generates a secret from django's utils
+              export SECRET_KEY=`python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'`
+              echo "SECRET_KEY='$SECRET_KEY'" > .env
+
+              echo "DJANGO_SUPERUSER_USERNAME=admin" >> .env
+              echo "DJANGO_SUPERUSER_PASSWORD=admin" >> .env
+              echo "DJANGO_SUPERUSER_EMAIL=root@localhost" >> .env
+              ALLOWED_HOSTS=""
+              for i in `ip addr | grep "inet " | awk '{print $2}' | awk -F/ '{print $1}'`
+              do
+                     ALLOWED_HOSTS="${i},${ALLOWED_HOSTS}"
+              done
+              MYHOST=`hostname`
+              ALLOWED_HOSTS=${MYHOST},${ALLOWED_HOSTS}
+              echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> .env
+
+              # append the db stuff if a file exists.
+              if [ -e "${RUNDIR}/env.db" ]
+              then
+                     cat ${RUNDIR}/env.db >> .env
+              else
+                     echo "No env.db, continuing with Sqlite"
+              fi
+
+              # other variables can be set directly in the ENV (for containers)
+              # or via the .env file.   See the .env.example for possible varialbes.
+              
+              # pull in a .env.custom if it exists.
+              if [ -e .env.custom ]
+              then 
+                     cat .env.custom >> .env
+              fi
+
+              python manage.py collectstatic --noinput
+
+              # grab a copy of busybox
+              wget -q -O static/busybox https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
+
+              touch /var/www/mprov_control_center/.dev.init
+
+       fi
+else
+       echo "Running in production mode"
+fi
+
+
 cd /var/www/mprov_control_center
 . bin/activate
 
@@ -6,11 +73,26 @@ cd /var/www/mprov_control_center
 python manage.py migrate
 
 # set up the secret key
-SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
-echo "SECRET_KEY=$SECRET_KEY" >> /var/www/mprov_control_center/.env
+# Check if the variable exists in the .env file
+NEW_VALUE=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') 
+if grep -q "^SECRET_KEY" /var/www/mprov_control_center/.env; then
+    # If it exists, update its value
+    sed -i "s/^SECRET_KEY=.*$/SECRET_KEY=${NEW_VALUE}/" /var/www/mprov_control_center/.env
+else
+    # If it doesn't exist, append it to the file
+    echo "SECRET_KEY=${NEW_VALUE}" >> /var/www/mprov_control_center/.env
+fi
 
 # let's adjust the env file based on the docker env
-echo "ALLOWED_HOSTS=$ALLOWED_HOSTS,$MPROV_MPCC_HOSTNAME,${MPROV_MPCC_HOSTNAME}.${MPROV_DOMAINNAME},$MPROV_MPCC_IPs" >> /var/www/mprov_control_center/.env
+
+NEW_VALUE="$MPROV_MPCC_HOSTNAME,${MPROV_MPCC_HOSTNAME}.${MPROV_DOMAINNAME},$MPROV_MPCC_IPs,${ALLOWED_HOSTS}"
+if grep -q "^ALLOWED_HOSTS=" /var/www/mprov_control_center/.env; then
+    # If it exists, update its value
+    sed -i "s/^ALLOWED_HOSTS=.*/ALLOWED_HOSTS=${NEW_VALUE}/" /var/www/mprov_control_center/.env
+else
+    # If it doesn't exist, append it to the file
+    echo "ALLOWED_HOSTS=${NEW_VALUE}" >> /var/www/mprov_control_center/.env
+fi
 
 if [ "$1" != "-d" ] 
 then
